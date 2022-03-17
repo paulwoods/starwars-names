44807d014d3cf83432b74768edf51e77df7d81fd
Paul Woods <mr.paul.woods@gmail.com>
2022-03-16 20:23:58 -0500

    Adding the library

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var uniqueRandomArray = require('unique-random-array');
+var starWarsNames = require('./starwars-names.json');
+
+
+
+
+module.exports = {
+    all: starWarsNames,
+    random: uniqueRandomArray(starWarsNames)
+}
